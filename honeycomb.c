9ba7c520a6685c1f64bd21f4b9ebba458701891e
kubik20083 <kubik20083@mail.ru>
2024-03-17 09:43:25 +0300

    dz added

---
+++ honeycomb.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf(" __    __ \n");
+    printf("/  \\__/  \\\n");
+    printf("\\__/  \\__/\n"); 
+    printf("/  \\__/  \\ \n");
+    printf("\\__/  \\__/\n");
+    return 0;
+}
